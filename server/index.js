119b240bfd9d103ff1923e8c6d869b393cd88abb
theinfinity1410 <swayamgosavi1410@gmail.com>
2025-06-15 10:48:14 +0530

    Cors enable

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // index.js
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routes from "./routes.js";
 
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/", routes);
 
 app.listen(PORT, () => {
